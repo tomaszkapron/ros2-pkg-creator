cef7264c67729247bff8ebba81626d4493deaf53
tomaszkapron <tomasz.a.kapron@gmail.com>
2022-11-29 11:16:00 +0100

    clean template pkg

--- autoware_auto_create_pkg/template/src/hello_world_node.cpp
+++ autoware_auto_create_pkg/template/src/hello_world_node.cpp
@@ -1,19 +1,3 @@
-// Copyright 2021 The Autoware Foundation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// Co-developed by Tier IV, Inc. and Apex.AI, Inc.
-
 #include "hello_world/hello_world_node.hpp"
 
 namespace autoware
@@ -38,6 +22,4 @@ int32_t HelloWorldNode::print_hello() const
 
 #include "rclcpp_components/register_node_macro.hpp"
 
-// This acts as an entry point, allowing the component to be
-// discoverable when its library is being loaded into a running process
 RCLCPP_COMPONENTS_REGISTER_NODE(autoware::hello_world::HelloWorldNode)
